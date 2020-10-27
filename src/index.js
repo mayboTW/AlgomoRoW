aca4ebac31f681f30ce16a45c6a1b6fed3a4bdd3
mayboTW <cmhuang.nctu@gmail.com>
2020-10-27 20:43:45 +0800

    Show random segments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-
-UIkit.util.on('#playbtn', 'click', function () {
-    console.log('Hello play button.');
+UIkit.util.on('#refreshbtn', 'click', function () {
+    const num = Math.floor(Math.random() * 10)  + 1;
+    $("#svgPane").empty();
+    var draw = SVG().addTo('#svgPane').size(1000, 300);
+    for (var i=0; i!=num; i++){
+        const h = Math.floor(Math.random() * 100) + 1;
+        var rect = draw.rect(10, h).fill('#80FF00').move(20 + i*20, 100-h);
+    }
+    console.log('#: ' + num);
 });
 
-UIkit.util.on('#stopbtn', 'click', function () {
-    console.log('Hello stop button.');
+UIkit.util.on('#deletebtn', 'click', function () {
+    console.log('clear');
+    $("#svgPane").empty();
 });
