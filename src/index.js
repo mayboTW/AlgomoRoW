bb57e123558b1c970b42b54d2374d0695e56cabc
mayboTW <cmhuang.nctu@gmail.com>
2020-10-28 20:51:24 +0800

    Extract bubble sort function + bindkey 'p' for play button

--- src/index.js
+++ src/index.js
@@ -6,6 +6,36 @@ UIkit.util.on('#refreshbtn', 'click', function () {
 
 UIkit.util.on('#playbtn', 'click', function () {
     console.log('Play');
+    bubbleSort();
+});
+
+UIkit.util.on('#deletebtn', 'click', function () {
+    console.log('clear');
+    $("#svgPane").empty();
+});
+
+document.onkeyup = function(e) {
+    if (e.key == 'r') {
+        refreshValues();
+    } else if (e.key == 'p') {
+        bubbleSort();
+    }
+};
+
+function refreshValues() {
+    const num = Math.floor(Math.random() * 10)  + 10;
+    $("#svgPane").empty();
+    values = [];
+    var draw = SVG().addTo('#svgPane').size(600, 300);
+    for (var i=0; i!=num; i++){
+        const h = Math.floor(Math.random() * 300) + 1;
+        values.push(h);
+        var rect = draw.rect(10, h).fill('#80FF00').move(20 + i*20, 300-h);
+    }
+    console.log('#: ' + num + ", Values: " + values);
+}
+
+function bubbleSort() {
     $("#svgPane").empty();
     for(var i=0; i!=values.length; ++i) {
         for(var j=i+1; j!=values.length; ++j) {
@@ -23,30 +53,4 @@ UIkit.util.on('#playbtn', 'click', function () {
         var rect = draw.rect(10, h).fill('#80FF00').move(20 + i*20, 300-h);
     }
     console.log('#: ' + values.length + ", Values: " + values);
-});
-
-UIkit.util.on('#deletebtn', 'click', function () {
-    console.log('clear');
-    $("#svgPane").empty();
-});
-
-document.onkeyup = function(e) {
-    if (e.key == 'r') {
-        refreshValues();
-    } else if (e.key == 'p') {
-
-    }
-};
-
-function refreshValues() {
-    const num = Math.floor(Math.random() * 10)  + 10;
-    $("#svgPane").empty();
-    values = [];
-    var draw = SVG().addTo('#svgPane').size(600, 300);
-    for (var i=0; i!=num; i++){
-        const h = Math.floor(Math.random() * 300) + 1;
-        values.push(h);
-        var rect = draw.rect(10, h).fill('#80FF00').move(20 + i*20, 300-h);
-    }
-    console.log('#: ' + num + ", Values: " + values);
 }
