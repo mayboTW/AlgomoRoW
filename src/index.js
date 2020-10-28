a0b476c9d5cc5def76447406556533d9860ffd53
mayboTW <cmhuang.nctu@gmail.com>
2020-10-28 20:05:37 +0800

    Sort values when click on play button

--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ UIkit.util.on('#refreshbtn', 'click', function () {
     refreshValues();
 });
 
+UIkit.util.on('#playbtn', 'click', function () {
+    console.log('Play');
+    $("#svgPane").empty();
+    for(var i=0; i!=values.length; ++i) {
+        for(var j=i+1; j!=values.length; ++j) {
+            if (values[i] > values[j]) {
+                var temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
+            }
+        }
+    }
+
+    var draw = SVG().addTo('#svgPane').size(1000, 300);
+    for (var i=0; i!=values.length; i++){
+        var h = values[i];
+        var rect = draw.rect(10, h).fill('#80FF00').move(20 + i*20, 100-h);
+    }
+    console.log('#: ' + values.length + ", Values: " + values);
+});
+
 UIkit.util.on('#deletebtn', 'click', function () {
     console.log('clear');
     $("#svgPane").empty();
@@ -16,7 +37,7 @@ document.onkeyup = function(e) {
 };
 
 function refreshValues() {
-    const num = Math.floor(Math.random() * 10)  + 1;
+    const num = Math.floor(Math.random() * 10)  + 10;
     $("#svgPane").empty();
     values = [];
     var draw = SVG().addTo('#svgPane').size(1000, 300);
