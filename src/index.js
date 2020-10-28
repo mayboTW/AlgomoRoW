2363f21e4e5ead7a692d198e7774e498bee43d72
mayboTW <cmhuang.nctu@gmail.com>
2020-10-28 19:47:52 +0800

    Rename + cache values

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+var values;
+
 UIkit.util.on('#refreshbtn', 'click', function () {
-    recreateSegments();
+    refreshValues();
 });
 
 UIkit.util.on('#deletebtn', 'click', function () {
@@ -9,17 +11,19 @@ UIkit.util.on('#deletebtn', 'click', function () {
 
 document.onkeyup = function(e) {
     if (e.shiftKey && e.which == 82) {
-        recreateSegments();
+        refreshValues();
     }
 };
 
-function recreateSegments() {
+function refreshValues() {
     const num = Math.floor(Math.random() * 10)  + 1;
     $("#svgPane").empty();
+    values = [];
     var draw = SVG().addTo('#svgPane').size(1000, 300);
     for (var i=0; i!=num; i++){
         const h = Math.floor(Math.random() * 100) + 1;
+        values.push(h);
         var rect = draw.rect(10, h).fill('#80FF00').move(20 + i*20, 100-h);
     }
-    console.log('#: ' + num);
+    console.log('#: ' + num + ", Values: " + values);
 }
